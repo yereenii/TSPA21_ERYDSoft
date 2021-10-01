fa8bc3cec358843a1242dc3c7949a544fe9e3828
Yerení JR Jiménez <32132969@uaz.edu.mx>
2021-10-01 14:49:53 -0500

    Se agrega calendario, se da formato al botón y círculos.

--- lib/pages/recordatorio.dart
+++ lib/pages/recordatorio.dart
@@ -1,8 +1,10 @@
+import 'package:diabits/pages/new_reminder_page.dart';
 import 'package:diabits/utils/responsive.dart';
 import 'package:diabits/widgets/circle.dart';
-import 'package:diabits/widgets/icon_container.dart';
 import 'package:diabits/widgets/calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
 
 
 class Recordatorio extends StatefulWidget {
@@ -31,14 +33,16 @@ class _RecordatorioState extends State<Recordatorio> {
             width: responsive.width,
             height: responsive.height,
             color: Colors.white,
+
             child: Stack(
               alignment: Alignment.center,
               children: <Widget>[
-
+                //aqui
+                Calendario(),
                 Positioned(
                   //circulo blue
-                  top: -blueSize * 0.4,
-                  right: -blueSize * 0.2,
+                  top: -blueSize * -2,
+                  right: -blueSize * -0.4,
                   child: Circle(
                     size: blueSize,
                     colors: [Colors.blue, Colors.blue],
@@ -46,8 +50,8 @@ class _RecordatorioState extends State<Recordatorio> {
                 ),
                 Positioned(
                   //Circulo cyan
-                  top: -tealAccentSize * 0.45,
-                  left: -tealAccentSize * 0.17,
+                  top: -tealAccentSize * -3,
+                  left: -tealAccentSize * -0.77,
                   child: Circle(
                     size: tealAccentSize,
                     colors: [
@@ -56,7 +60,26 @@ class _RecordatorioState extends State<Recordatorio> {
                     ],
                   ),
                 ),
-                Calendario(),
+
+                SizedBox(height: responsive.dp(5)),
+                SizedBox(
+                  width: responsive.width * 0.62,
+                  child: MaterialButton(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    child: Text(
+                      'Nuevo Recordatorio',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: responsive.dp(2),
+                      ),
+                    ),
+                    onPressed: ()=> NewReminderPage(),
+                    color: Colors.blue.shade800,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
               ],
             ),
             //child: Stack(children: <Widget>[IconContainer()]),
--- lib/widgets/calendar.dart
+++ lib/widgets/calendar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:diabits/utils/responsive.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
 
 class Calendario extends StatefulWidget {
   Calendario({Key? key}) : super(key: key);
@@ -8,11 +10,32 @@ class Calendario extends StatefulWidget {
   _CalendarioState createState() => _CalendarioState();
 }
 
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    localizationsDelegates: [
+      // ... app-specific localization delegate[s] here
+      SfGlobalLocalizations.delegate
+    ],
+    supportedLocales: [
+      const Locale('es'),
+      const Locale.fromSubtags(languageCode: 'es'),
+      // ... other locales the app supports
+    ],
+    locale: const Locale('es'),
+
+    home: Calendario(),
+  );
+}
+
 class _CalendarioState extends State<Calendario> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SfCalendar(
+    final Responsive responsive = Responsive.of(context);
+    return Container(
+      width : 400,
+      height: 400,
+      child: SfCalendar(
         view: CalendarView.month,
         firstDayOfWeek: 7,
       ),
