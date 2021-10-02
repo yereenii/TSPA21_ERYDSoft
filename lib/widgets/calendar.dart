39d74a68c218745db2de9e4d2b8753e45b33628d
Eladio <juarez_ejp@live.com>
2021-10-02 18:57:59 -0500

    ruta a nuevo recordatorio actualizada

--- lib/widgets/calendar.dart
+++ lib/widgets/calendar.dart
@@ -32,63 +32,59 @@ class _CalendarioState extends State<Calendario> {
   GlobalKey<FormState> _formKey = GlobalKey();
 
   _summit() {
-    Navigator.pushNamed(context, 'recordatorios');
+    Navigator.pushNamed(context, 'nuevorecordatorio');
   }
 
   @override
   Widget build(BuildContext context) {
     final Responsive responsive = Responsive.of(context);
     return Container(
-       // width: 400,
+        // width: 400,
         child: Column(
-          children: <Widget>[
-            SizedBox(height: responsive.dp(2)),
-            SfCalendar(
-              view: CalendarView.month,
-            monthViewSettings: MonthViewSettings(
+      children: <Widget>[
+        SizedBox(height: responsive.dp(2)),
+        SfCalendar(
+          view: CalendarView.month,
+          monthViewSettings: MonthViewSettings(
             showAgenda: true,
-            ),
-              firstDayOfWeek: 7,
-              timeRegionBuilder: (context, timeRegionDetails) =>
-                  _build(context),
-              dataSource: FechaRecordatorioSource(getAppointments()),
-            ),
+          ),
+          firstDayOfWeek: 7,
+          timeRegionBuilder: (context, timeRegionDetails) => _build(context),
+          dataSource: FechaRecordatorioSource(getAppointments()),
+        ),
 
-            //aqui mero
+        //aqui mero
 
-
-
-
-            SizedBox(height: responsive.dp(2)),
-            SizedBox(
-              width: responsive.width * 0.62,
-              child: MaterialButton(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: Text(
-                  'Nuevo Recordatorio',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: responsive.dp(2),
-                  ),
-                ),
-                onPressed: _summit,
-                color: Colors.blue.shade800,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
+        SizedBox(height: responsive.dp(2)),
+        SizedBox(
+          width: responsive.width * 0.62,
+          child: MaterialButton(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            child: Text(
+              'Nuevo Recordatorio',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: responsive.dp(2),
               ),
             ),
-          ],
-        ));
+            onPressed: _summit,
+            color: Colors.blue.shade800,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 }
 
-List<Appointment> getAppointments(){
+List<Appointment> getAppointments() {
   //Arreglo para los recordatorios
   List<Appointment> recordatorios = <Appointment>[];
-  final DateTime today= DateTime.now();
+  final DateTime today = DateTime.now();
   final DateTime startTime =
-  DateTime(today.year, today.month, today.day, 22, 0, 0);
+      DateTime(today.year, today.month, today.day, 22, 0, 0);
   final DateTime endTime = startTime.add(const Duration(hours: 1));
 
   recordatorios.add(Appointment(
@@ -102,9 +98,8 @@ List<Appointment> getAppointments(){
   return recordatorios;
 }
 
-
-class FechaRecordatorioSource extends CalendarDataSource{
-  FechaRecordatorioSource(List<Appointment> source){
+class FechaRecordatorioSource extends CalendarDataSource {
+  FechaRecordatorioSource(List<Appointment> source) {
     appointments = source;
   }
 }
