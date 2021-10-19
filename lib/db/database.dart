a698e16dea52b363bc62a5b543f93ab504617ae5
RichLoMx <rjl_17@hotmail.com>
2021-10-19 18:49:03 -0500

    se modifico las clases database. dart y la clase operation.dart para crear la tabla que almacena los datos de los alimentos

--- lib/db/database.dart
+++ lib/db/database.dart
@@ -2,36 +2,36 @@ import 'package:sqflite/sqflite.dart';
 import 'dart:async';
 
 class Login {
-  final String s="";
+  final String s = "";
 }
 
 class diabitsDatabase {
   late Database db;
 
   initDb() async {
-    db = await openDatabase(
-      'my_db.db', 
-      version: 1,
-      onCreate: (
-        Database db, 
-        int version,
-      ) {
+    db = await openDatabase('my_db.db', version: 1, onCreate: (
+      Database db,
+      int version,
+    ) {
       db.execute(
-        "CREATE TABLE usuarios (id_usuario INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre VARCHAR2 NOT NULL, correo VARCHAR2 NOT NULL, password VARCHAR2 NOT NULL);"
-        "CREATE TABLE recordatorios (id_recordatorios INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre_recordatorio VARCHAR2 NOT NULL, fecha DATE NOT NULL, FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario) );"
-      );    
+          "CREATE TABLE usuarios (id_usuario INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre VARCHAR2 NOT NULL, correo VARCHAR2 NOT NULL, password VARCHAR2 NOT NULL);"
+          "CREATE TABLE recordatorios (id_recordatorios INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre_recordatorio VARCHAR2 NOT NULL, fecha DATE NOT NULL, FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario) );");
     });
-    
-    db = await openDatabase(
-      'my_db.db', 
-      version: 2,
-      onCreate: (
-        Database db, 
-        int version,
-      ) {
+
+    db = await openDatabase('my_db.db', version: 2, onCreate: (
+      Database db,
+      int version,
+    ) {
       db.execute(
-        "CREATE TABLE recordatorios (id_recordatorios INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre_recordatorio VARCHAR2 NOT NULL, fecha DATE NOT NULL, FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario) );"
-      );    
+          "CREATE TABLE recordatorios (id_recordatorios INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre_recordatorio VARCHAR2 NOT NULL, fecha DATE NOT NULL, FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario) );");
+    });
+
+    db = await openDatabase('my_db.db', version: 3, onCreate: (
+      Database db,
+      int version,
+    ) {
+      db.execute(
+          "CREATE TABLE alimentos (id_alimento INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre_alimento VARCHAR2 NOT NULL, nota VARCHAR2 NOT NULL, harmfull BOOLEAN, FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuario) );");
     });
   }
 }
--- lib/db/operation.dart
+++ lib/db/operation.dart
@@ -1,26 +1,35 @@
 import 'package:diabits/models/recordatorio.dart';
 import 'package:diabits/models/usuario.dart';
+import 'package:diabits/models/alimento.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 import 'dart:async';
 
 class OperationDB {
   static List<Recordatorio> listaRecordatorios = [];
+  static List<Alimento> listaAlimentos = [];
   bool _existeUsuario = false;
 // Abre la base de datos y guarda la referencia.
   Future<Database> _openDB() async {
     // Establecer la ruta a la base de datos. Nota: Usando la función `join` del
     // complemento `path` es la mejor práctica para asegurar que la ruta sea correctamente
     // construida para cada plataforma.
+    String qUsuario =
+        "CREATE TABLE usuarios (id_usuario INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre VARCHAR2 NOT NULL, correo VARCHAR2 NOT NULL, password VARCHAR2 NOT NULL);";
+    String qRecordatorio =
+        "CREATE TABLE recordatorios (id_recordatorio INTEGER PRIMARY KEY, nombre_recordatorio VARCHAR2 NOT NULL, fecha Date NOT NULL);";
+    String qAlimento =
+        "CREATE TABLE alimentos (id_alimento INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre_alimento VARCHAR2 NOT NULL, nota VARCHAR2 NOT NULL, danino BOOLEAN);";
+    List<String> qCreates = [qUsuario, qRecordatorio, qAlimento];
     return openDatabase(
       join(await getDatabasesPath(), 'db_diabitss.db'),
-      onCreate: (db, version) {
-        return db.execute(
-            // Ejecuta la sentencia CREATE TABLE en la base de datos
-            //"CREATE TABLE usuarios (id_usuario INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre VARCHAR2 NOT NULL, correo VARCHAR2 NOT NULL, password VARCHAR2 NOT NULL);" +
-            "CREATE TABLE recordatorios (id_recordatorio INTEGER PRIMARY KEY, nombre_recordatorio VARCHAR2 NOT NULL, fecha Date NOT NULL);");
+      onCreate: (db, version) async {
+        // Ejecuta la sentencia CREATE TABLE en la base de datos
+        for (String query in qCreates) {
+          await db.execute(query);
+        }
       },
-      version: 4,
+      version: 5,
     );
   }
 
@@ -106,5 +115,46 @@ class OperationDB {
             id_recordatorio: recordatorioMap[i]['id_recordatorio'],
             nombre_recordatorio: recordatorioMap[i]['nombre_recordatorio'],
             fecha: DateTime.parse((recordatorioMap[i]['fecha']))));
+  }
+
+  Future<int> inserAlimento(Alimento alimento) async {
+    Database database = await _openDB();
+    return database.insert('alimentos', alimento.toMap());
+  }
+
+  bool toBoolean(String str, [bool strict = false]) {
+    if (strict == true) {
+      return str == '1' || str == 'true';
+    }
+    return str != '0' && str != 'false' && str != '';
+  }
+
+  Future<List<Alimento>> getAlimentos() async {
+    Database database = await _openDB();
+    final List<Map<String, dynamic>> alimentoMap =
+        await database.query("alimentos");
+    for (var n in alimentoMap) {
+      listaAlimentos.add(Alimento(
+          idAlimento: n['id_alimento'],
+          nombreAlimento: n['nombre_alimento'],
+          nota: n['nota'],
+          danino: toBoolean(n['danino'])));
+      print("_____" +
+          n['id_alimento'].toString() +
+          "_" +
+          n['nombe_alimento'].toString() +
+          "_" +
+          n['nota'].toString() +
+          "_" +
+          n['danino'].toString());
+    }
+
+    return List.generate(
+        alimentoMap.length,
+        (i) => Alimento(
+            idAlimento: alimentoMap[i]['id_alimento'],
+            nombreAlimento: alimentoMap[i]['nombre_alimento'],
+            nota: alimentoMap[i]['nota'],
+            danino: alimentoMap[i]['danino']));
   }
 }
