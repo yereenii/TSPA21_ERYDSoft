66548abbf81cdc31c768f10c4084ab1c17e9f4b4
RichLoMx <rjl_17@hotmail.com>
2021-10-19 19:15:58 -0500

    midificacion de la clase home_start_form para que no salten esceptions de containers, se modifico la clase inkWellTabs para agregar los atributos para el fondo de color y el color de las letras del mismo

--- lib/widgets/home_start_form.dart
+++ lib/widgets/home_start_form.dart
@@ -22,85 +22,67 @@ class _HomeStartForm extends State<HomeStartForm> {
   void initState() {
     _recordatorioController.text = "Recordatorios";
     _alimentoController.text = "Alimentos";
-    _sabiasQueController.text = "¿Sabias que?";
+    _sabiasQueController.text = "¿Sabías que?";
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final Responsive responsive = Responsive.of(context);
-    return Positioned(
-      child: Container(
-        constraints: BoxConstraints(
-          //ancho del dispositivo si es tablet 430 si no ancho-30
-          maxWidth: responsive.isTablet ? 430 : responsive.width - 30,
-        ),
-        child: Form(
-          key: _formKey, //ayuda a validar los campos. se usa en sumit
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: responsive.dp(2)),
-              /*************************************** */
-              SizedBox(
-                width: responsive.width * 0.50,
-                child: Column(
-                  children: [
-                    InkTextFormField(
-                      onTap: () async =>
-                          Navigator.pushNamed(context, 'recordatorio'),
-                      label: "",
-                      controller: _recordatorioController,
-                      fontSize: responsive.dp(responsive.isTablet ? 1.9 : 1.6),
-                      width: responsive.width / 1.7,
-                      heigth: responsive.height / 15,
-                    ),
-                  ],
-                ),
-              ),
-
-              /*************************************** */
-              SizedBox(height: responsive.dp(2)),
-              /*************************************** */
-              SizedBox(
-                width: responsive.width * 0.50,
-                child: Column(
-                  children: [
-                    InkTextFormField(
-                       onTap: () async =>
-                          Navigator.pushNamed(context, 'alimentos'),
-                      label: "",
-                      controller: _alimentoController,
-                      fontSize: responsive.dp(responsive.isTablet ? 1.9 : 1.6),
-                      width: responsive.width / 1.7,
-                      heigth: responsive.height / 15,
-                    ),
-                  ],
-                ),
-              ),
-              /*************************************** */
-              SizedBox(height: responsive.dp(2)),
-              /*************************************** */
-              SizedBox(
-                width: responsive.width * 0.50,
-                child: Column(
-                  children: [
-                    InkTextFormField(
-                      //onTap: () async => _selectTime(context),
-                      label: "",
-                      controller: _sabiasQueController,
-                      fontSize: responsive.dp(responsive.isTablet ? 1.9 : 1.6),
-                      width: responsive.width / 1.7,
-                      heigth: responsive.height / 15,
-                    ),
-                  ],
-                ),
-              ),
-              /*************************************** */
-              SizedBox(height: responsive.dp(2)),
-              SizedBox(height: responsive.dp(2)),
-            ],
+    return Container(
+      constraints: BoxConstraints(
+        //ancho del dispositivo si es tablet 430 si no ancho-30
+        maxWidth: responsive.isTablet ? 430 : responsive.width - 30,
+      ),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: responsive.dp(2)),
+          /*************************************** */
+          SizedBox(
+            width: responsive.width * 0.50,
+            child: InkTextFormField(
+              fillColor: Colors.tealAccent.shade400,
+              onTap: () async => Navigator.pushNamed(context, 'recordatorio'),
+              label: "",
+              controller: _recordatorioController,
+              fontSize: responsive.dp(responsive.isTablet ? 1.9 : 1.6),
+              width: responsive.width / 1.7,
+              heigth: responsive.height / 15,
+            ),
           ),
-        ),
+
+          /*************************************** */
+          SizedBox(height: responsive.dp(2)),
+          /*************************************** */
+          SizedBox(
+            width: responsive.width * 0.50,
+            child: InkTextFormField(
+              fillColor: Colors.tealAccent.shade400,
+              onTap: () async => Navigator.pushNamed(context, 'alimentos'),
+              label: "",
+              controller: _alimentoController,
+              fontSize: responsive.dp(responsive.isTablet ? 1.9 : 1.6),
+              width: responsive.width / 1.7,
+              heigth: responsive.height / 15,
+            ),
+          ),
+          /*************************************** */
+          SizedBox(height: responsive.dp(2)),
+          /*************************************** */
+          SizedBox(
+            width: responsive.width * 0.50,
+            child: InkTextFormField(
+              fillColor: Colors.tealAccent.shade400,
+              onTap: () async => Navigator.pushNamed(context, 'nuevoalimento'),
+              label: "",
+              controller: _sabiasQueController,
+              fontSize: responsive.dp(responsive.isTablet ? 1.9 : 1.6),
+              width: responsive.width / 1.7,
+              heigth: responsive.height / 15,
+            ),
+          ),
+          /*************************************** */
+        ],
       ),
     );
   }
--- lib/widgets/inkWellTabs.dart
+++ lib/widgets/inkWellTabs.dart
@@ -8,6 +8,8 @@ class InkTextFormField extends StatelessWidget {
   final double? heigth;
   final double? fontSize;
   final String? label;
+  final Color? fillColor;
+  final Color? fontColor;
 
   const InkTextFormField(
       {Key? key,
@@ -16,7 +18,9 @@ class InkTextFormField extends StatelessWidget {
       this.width,
       this.heigth,
       this.fontSize,
-      this.label})
+      this.label,
+      this.fillColor,
+      this.fontColor})
       : super(key: key);
 
   @override
@@ -24,31 +28,37 @@ class InkTextFormField extends StatelessWidget {
     return Column(
       children: <Widget>[
         Text(label!),
-        InkWell(
-          onTap: onTap,
-          child: InputDecorator(
-            decoration: const InputDecoration(enabled: true),
-            child: Localizations(
-              locale: const Locale('en', 'US'),
-              delegates: const <LocalizationsDelegate<dynamic>>[
-                DefaultWidgetsLocalizations.delegate,
-                DefaultMaterialLocalizations.delegate,
-              ],
-              child: MediaQuery(
-                data: const MediaQueryData(),
-                child: Directionality(
-                  textDirection: TextDirection.ltr,
-                  child: TextFormField(
-                    style: TextStyle(fontSize: fontSize),
-                    textAlign: TextAlign.center,
-                    enabled: false,
-                    keyboardType: TextInputType.text,
-                    controller: controller,
-                    onSaved: (String? val) {},
-                    decoration: const InputDecoration(
-                      disabledBorder:
-                          UnderlineInputBorder(borderSide: BorderSide.none),
-                      contentPadding: EdgeInsets.only(top: 0.0),
+        Material(
+          color: fillColor,
+          child: InkWell(
+            highlightColor: Colors.red,
+            onTap: onTap,
+            child: InputDecorator(
+              decoration: const InputDecoration(
+                enabled: true,
+              ),
+              child: Localizations(
+                locale: const Locale('en', 'US'),
+                delegates: const <LocalizationsDelegate<dynamic>>[
+                  DefaultWidgetsLocalizations.delegate,
+                  DefaultMaterialLocalizations.delegate,
+                ],
+                child: MediaQuery(
+                  data: const MediaQueryData(),
+                  child: Directionality(
+                    textDirection: TextDirection.ltr,
+                    child: TextFormField(
+                      style: TextStyle(fontSize: fontSize, color: fontColor),
+                      textAlign: TextAlign.center,
+                      enabled: false,
+                      keyboardType: TextInputType.text,
+                      controller: controller,
+                      onSaved: (String? val) {},
+                      decoration: const InputDecoration(
+                        disabledBorder:
+                            UnderlineInputBorder(borderSide: BorderSide.none),
+                        contentPadding: EdgeInsets.only(top: 0.0),
+                      ),
                     ),
                   ),
                 ),
