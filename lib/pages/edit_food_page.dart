bb353ac5eeb499c3d50ccb83b8bf55fadaf6a88b
Yerení JR Jiménez <32132969@uaz.edu.mx>
2021-10-22 13:28:59 -0500

    Se agregan archivos para la edición de un elemento

---
+++ lib/pages/edit_food_page.dart
@@ -0,0 +1,65 @@
+import 'package:diabits/utils/responsive.dart';
+import 'package:diabits/widgets/circle.dart';
+import 'package:diabits/widgets/circles.dart';
+import 'package:diabits/widgets/edit_food_form.dart';
+import 'package:flutter/material.dart';
+
+//sft
+class EditFoodPage extends StatefulWidget {
+  static const routeName = 'editaAlimento';
+  //const NewFoodPage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<EditFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    final Responsive responsive = Responsive.of(context);
+    final double blueSize = responsive.wp(80); //80% del ancho del dispo
+    final double tealAccentSize = responsive.wp(60);
+
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          //si da click fuera
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          //la altura es dinamica calculada a los elementos hijos
+          child: Container(
+            width: responsive.width,
+            height: responsive.height,
+            color: Colors.white,
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Positioned(
+                  top: responsive.wp(.01),
+                  child: const Circles(),
+                ),
+                Positioned(
+                  top: responsive.height * 0.10,
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "Edita Alimento",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: responsive.dp(1.8),
+                        ),
+                      ),
+                      SizedBox(height: responsive.dp(20)),
+                      const EditFoodForm(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/edit_food_form.dart
@@ -0,0 +1,157 @@
+// ignore_for_file: avoid_print
+import 'package:diabits/db/operation.dart';
+import 'package:diabits/models/alimento.dart';
+import 'package:diabits/utils/responsive.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'Input_text.dart';
+
+class EditFoodForm extends StatefulWidget {
+  const EditFoodForm({Key? key}) : super(key: key);
+
+  @override
+  _EditFoodForm createState() => _EditFoodForm();
+}
+
+class _EditFoodForm extends State<EditFoodForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  String _nombre = "";
+  String _descripcion = "";
+  bool _harmful = false;
+  OperationDB _operationDB = OperationDB();
+
+  _summit() {
+    //metodo que ejecuta las validaciones que pongas en el form
+    final isoOK = _formKey.currentState!.validate();
+    print("form isOK $isoOK");
+    if (isoOK) {
+      print(_nombre);
+      print(_descripcion);
+      print(_harmful);
+      _insertarAlimento();
+      _regresarAlimentos();
+    }
+  }
+
+  _insertarAlimento() {
+    _operationDB.editA(Alimento(
+        nombreAlimento: _nombre, nota: _descripcion, danino: _harmful));
+    //_operationDB.getAlimentos();
+  }
+
+  _regresarAlimentos() {
+    Navigator.pop(context, 'alimentos');
+  }
+
+  @override
+  void initState() {
+    _harmful = false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Responsive responsive = Responsive.of(context);
+    return Container(
+      constraints: BoxConstraints(
+        //ancho del dispositivo si es tablet 430 si no ancho-30
+        maxWidth: responsive.isTablet ? 430 : responsive.width - 30,
+      ),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            InputText(
+              keyboardType: TextInputType.emailAddress,
+              fontSize: responsive.dp(responsive.isTablet ? 1.9 : 1.6),
+              onChanged: (text) {
+                _nombre = text;
+              },
+              validator: (text) {
+                if (text!.isEmpty) {
+                  return "Nombre inválido";
+                }
+                return null;
+              },
+              label: 'Nombre del Alimento',
+            ),
+            SizedBox(height: responsive.dp(2)),
+            /*************************************** */
+            InputText(
+              keyboardType: TextInputType.emailAddress,
+              fontSize: responsive.dp(responsive.isTablet ? 1.9 : 1.6),
+              onChanged: (text) {
+                _descripcion = text;
+              },
+              validator: (text) {
+                if (text!.isEmpty) {
+                  return "Dscripción inválida";
+                }
+                return null;
+              },
+              label: 'Descripcion',
+            ),
+            /*************************************** */
+            SizedBox(height: responsive.dp(2)),
+            /*************************************** */
+            SizedBox(
+              width: responsive.width * 1,
+              child: Row(
+                children: <Widget>[
+                  const Text('Dañino'),
+                  Checkbox(
+                      value: _harmful,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _harmful = value!;
+                        });
+                      })
+                ],
+              ),
+            ),
+            /*************************************** */
+            SizedBox(height: responsive.dp(2)),
+            SizedBox(
+              width: responsive.width * 0.50,
+              child: MaterialButton(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: Text(
+                  'Editar',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: responsive.dp(2),
+                  ),
+                ),
+                onPressed: _summit,
+                color: Colors.blue.shade800,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox(height: responsive.dp(2)),
+            SizedBox(
+              width: responsive.width * 0.50,
+              child: MaterialButton(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                child: Text(
+                  'Cancelar',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: responsive.dp(2),
+                  ),
+                ),
+                onPressed: _regresarAlimentos,
+                color: Colors.red.shade800,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox(height: responsive.dp(2)),
+          ],
+        ),
+      ),
+    );
+  }
+}
